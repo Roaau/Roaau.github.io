a11ecff3d65d80f5f0c565adde6fc59a84a71643
Roaau <159199777+Roaau@users.noreply.github.com>
2024-02-16 17:34:31 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+<script>
+  function cambia_de_pagina(){
+    alert("Gracias por visitar nuestra pagina, nos vamos a el mirador de la serrania")
+    location.href="https://roaau-javascript.web.app"
+  }
+</script>
